10750d92bab7bb936fd9128ee2b81e844218fcd0
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-10-16 00:43:18 -0400

    Fixing src/main/java/com/sensepost/log4jpwn/App.java for finding 2

--- src/main/java/com/sensepost/log4jpwn/App.java
+++ src/main/java/com/sensepost/log4jpwn/App.java
@@ -13,17 +13,17 @@ public class App {
         port(8080);
 
 (req, res) -> {
+
     String ua = req.headers("User-Agent");
     String pwn = req.queryParams("pwn");
     String pth = req.pathInfo();
 
-    logger.error("logging ua: {}", ua);
-    logger.error("logging pwn: {}", pwn);
-    logger.error("logging pth: {}", pth);
+    logger.error("logging ua: " + ua);
+    logger.error("logging pwn: " + pwn);
+    logger.error("logging pth: " + pth);
 
     return "ok: ua: " + ua + " " + "pwn: " + pwn + " pth:" + pth;
 }
 
-    }
-}
+
 

0fe9236af9cf5ba0ac90f4649ee74530dce55e9d
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-10-16 01:06:47 -0400

    Fixing src/main/java/com/sensepost/log4jpwn/App.java for finding 71

--- src/main/java/com/sensepost/log4jpwn/App.java
+++ src/main/java/com/sensepost/log4jpwn/App.java
@@ -13,20 +13,21 @@ public class App {
         port(8080);
 
 (req, res) -> {
-        String ua = req.request().headers("User-Agent");
-        String pwn = req.queryParams("pwn");
-        String pth = req.pathInfo();
+    String ua = req.headers("User-Agent");
+    String pwn = req.queryParams("pwn");
+    String pth = req.pathInfo();
 
-// Use a logging framework that supports masking or encryption
-        Logger logger = LogManager.getLogger(App.class);
-        logger.error("User-Agent: {}, pwn: {}, pth: {}", ua, pwn, pth);
+    Logger logger = LogManager.getLogger(App.class);
+    logger.error("User-Agent: {}, pwn: {}, pth: {}", Masker.mask(ua), Masker.mask(pwn), Masker.mask(pth));
 
-        return "ok: ua: " + ua + " " + "pwn: " + pwn + " pth:" + pth;
-    }
+    return "ok: ua: " + Masker.mask(ua) + " " + "pwn: " + Masker.mask(pwn) + " pth:" + Masker.mask(pth);
+}
+
 
 
     }
 }
+
 
 
 
